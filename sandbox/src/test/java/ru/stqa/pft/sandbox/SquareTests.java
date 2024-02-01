3d5f88f5b7f6d83640ef60ccb62e2996a8dcb0a2
sholize <kenesiz9000@gmail.com>
2024-02-01 11:40:40 +0300

    первый тест

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+import org.junit.jupiter.api.Test;
+
+public class SquareTests {
+
+    @Test
+    public void tesArea() {
+         int a = 5;
+         int b = 6;
+         assert a != b;
+    }
+}
